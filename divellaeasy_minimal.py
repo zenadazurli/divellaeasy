d2d816b7a4ed6617b41d4183746568e71d417f8f
zenadazurli <zenadazurli@libero.it>
2026-03-06 20:00:58 +0100

    Update divellaeasy_minimal.py

--- divellaeasy_minimal.py
+++ divellaeasy_minimal.py
@@ -17,8 +17,8 @@ REQUEST_TIMEOUT = 15
 ERRORI_DIR = "errori"  # cartella dove salvare gli errori
 
 # ================ DATI ACCOUNT =====================
-UID = "2288011"
-COOKIE_SESIDS = "KTeaAXDgWL"  # <-- SOSTITUISCI QUANDO SCADE
+UID = "2288903"
+COOKIE_SESIDS = "qZSoh3cHab"  # <-- SOSTITUISCI QUANDO SCADE
 COOKIE_STRING = f"sesids={COOKIE_SESIDS}; user_id={UID}"
 
 # ================ GLOBALS =====================
@@ -279,6 +279,7 @@ def main():
 if __name__ == "__main__":
     main()
     log("🏁 Script terminato")
+
 
 
 
